46a7b50dcd26b9520629a8f43be4fc7c88217f88
Muzammilsheriff <84589950+Muzammilsheriff@users.noreply.github.com>
2024-12-11 17:11:01 +0530

    Add files via upload

---
+++ JS/Function.js
@@ -0,0 +1,24 @@
+// Functions
+// Function Declaration
+function add(a, b) {  // with parameters
+    var total = a + b;
+    document.write(total);
+    return total; // return value parameter
+}
+
+
+function sub() {  // no parameters
+    document.write("Hello");
+    document.write("Hello");
+}
+
+sub();
+sub();
+
+add(2, 3);
+
+
+var total = add(2, 3);  // for return function
+document.write(total);
+
+
---
+++ JS/Loops.js
@@ -0,0 +1,25 @@
+// Loops
+
+// while loop
+
+var i = 0;
+while (i < 10) {  // entry check loop
+    console.log(i);
+    i++;
+}
+
+// do while loop
+
+var i = 11;
+do {   // exit check loop
+    console.log(i);
+    i++;
+} while (i < 10);
+
+
+// for loop
+
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
